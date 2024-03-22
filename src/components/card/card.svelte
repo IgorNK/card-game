415abfc65b9bceb43f8a42ae6acb3b79af9f12cf
IgorNK <117646556+IgorNK@users.noreply.github.com>
2024-03-22 11:14:28 +0000

    init

---
+++ src/components/card/card.svelte
@@ -0,0 +1,144 @@
+<script lang="ts">
+  export let title = "Rend";
+  export let flipped = false;
+</script>
+
+<button
+  class="card"
+  style:transform={flipped ? 'rotateY(180deg)' : ''}
+  on:click={() => flipped = !flipped}
+>
+  <div class="front">
+    <div class="price">
+      <slot name="price">
+        <span class="symbol-green">♠</span>
+        <span class="symbol-blue">♠</span>
+      </slot>
+    </div>
+    <div class="title">
+      <i>{title}</i>
+    </div>
+    <div class="image">
+      <slot name="image">
+        <span class="symbol">♠</span>
+      </slot>
+    </div>
+    <div class="description">
+      <slot name="description" class="description">
+        <p>Attack: 1</p>
+      </slot>
+    </div>
+  </div>
+  <div class="back">
+    <div class="pattern"></div>
+  </div>
+</button>
+
+<style>
+  .card {
+    position: relative;
+    aspect-ratio: 2.5 / 3.5;
+    font-size: min(2vw, 3.5vh, 1.5rem);
+    height: 18em;
+    background: var(--color-bg-0);
+    border-radius: 1em;
+    transform: rotateY(0);
+    transition: transform 0.4s;
+    transform-style: preserve-3d;
+    padding: 0;
+    user-select: none;
+    cursor: pointer;
+  }
+
+  .card.flipped {
+    transform: rotateY(180deg);
+  }
+
+  .front, .back {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    backface-visibility: hidden;
+    border-radius: 1em;
+    border: 1px solid var(--color-theme-2);
+    box-sizing: border-box;
+    padding: 1em;
+    padding-top: 0.5em;
+  }
+
+  .front {
+    background-color: var(--color-bg-2);
+    display: flex;
+    flex-direction: column;
+  }
+
+  .back {
+    transform: rotateY(180deg);
+  }
+
+  .symbol {
+    font-size: 10em;
+    color: var(--color-theme-1);
+  }
+
+  .symbol-green {    
+    color: var(--color-theme-3);
+  }
+
+  .symbol-blue {
+    color: var(--color-theme-2);
+  }
+  
+  .pattern {
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-bg-1);
+    /* pattern from https://projects.verou.me/css3patterns/#marrakesh */
+    background-image:
+    radial-gradient(var(--color-bg-2) 0.45em, transparent 0.5em),
+    repeating-radial-gradient(var(--color-bg-2) 0, var(--color-bg-2) 0.2em, transparent 0.25em, transparent 1em, var(--color-bg-2) 1.05em, var(--color-bg-2) 1.05em, transparent 1.3em, transparent 2.5em);
+    background-size: 1.5em 1.5em, 4.5em 4.5em;
+    background-position: 0 0;
+    border-radius: 1em;
+  }
+
+  .title {
+    font-weight: 700;
+  }
+
+  .price {
+    display: flex;
+    column-gap: 0.2em;
+    flex-direction: row;
+    height: 10%;
+    width: 100%;
+    align-items: flex-start;
+    justify-content: flex-end;
+    font-size: 1em;
+  }
+
+  .image {
+    height: 50%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .description {
+    height: 50%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: center;
+    text-align: left;
+  }
+  
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Card from "../components/card/card.svelte";
+</script>
+
+<svelte:head>
+	<title>Cards</title>
+	<meta name="description" content="Card demo app" />
+</svelte:head>
+
+<section>
+  <Card />
+</section>
+
+<style>
+  section {
+    perspective: 100vh;
+  }
+</style>
